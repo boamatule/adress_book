8b0fa09744957350087dc1d6b48705a96d4d4394
Boa Matule <boa.matule@gmail.com>
2019-06-19 01:25:54 +0200

    Adds step definition for see 'Contacts' in the homepage; Cleans the home page scalfolded automatically and adds it with new data to dispplay contacts; Test green.

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -1,12 +1,16 @@
 const  { After, Given, Then, When }  =  require('cucumber')
 
+
+After(async function()  {
+  return await this.closeHomePage()
+});
+
 Given('I visit the site', async function () {
   return  await this.openHomePage ()
 });
 
-Then('I should see {string}', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then('I should see {string}', async function(content) {
+  return await this.pageHasTextContent(content)
 });
 
 
@@ -22,37 +26,37 @@ Then('I fill in {string} with {string}', function (string, string2) {
 });
 
 
-Then('I fill in {string} with {string}', function (string, string2) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
+// Then('I fill in {string} with {string}', function (string, string2) {
+//   // Write code here that turns the phrase above into concrete actions
+//   return 'pending';
+// });
 
 
-Then('I fill in {string} with {string}', function (string, string2) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
+// Then('I fill in {string} with {string}', function (string, string2) {
+//   // Write code here that turns the phrase above into concrete actions
+//   return 'pending';
+// });
 
 
-Then('I fill in {string} with {string}', function (string, string2) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
+// Then('I fill in {string} with {string}', function (string, string2) {
+//   // Write code here that turns the phrase above into concrete actions
+//   return 'pending';
+// });
 
 
-Then('I fill in {string} with {string}', function (string, string2) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
+// Then('I fill in {string} with {string}', function (string, string2) {
+//   // Write code here that turns the phrase above into concrete actions
+//   return 'pending';
+// });
 
 
-Then('I click {string}', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
+// Then('I click {string}', function (string) {
+//   // Write code here that turns the phrase above into concrete actions
+//   return 'pending';
+// });
 
 
-Then('I should see {string}', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
+// Then('I should see {string}', function (string) {
+//   // Write code here that turns the phrase above into concrete actions
+//   return 'pending';
+// });
--- features/support/world.js
+++ features/support/world.js
@@ -13,6 +13,17 @@ class AdressBook {
 
     await this.page.goto(HOME_PAGE);
   }
+
+  async closeHomePage()  {
+    await this.browser.close()
+  }
+
+  async pageHasTextContent(expectedContent) {
+    const pageContent = await this.page.content();
+    const actualContent = pageContent.match(expectedContent)[0];
+
+    expect(actualContent).to.be.eq(expectedContent);
+  }
 }
 
 setWorldConstructor(AdressBook);
